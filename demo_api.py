f46a4be0bf8f22622dc35b8f63f1bf8b30bf927f
jileshl <jileshl@gmail.com>
2013-10-30 18:45:05 +0530

    Update demo_api.py

--- demo_api.py
+++ demo_api.py
@@ -37,3 +37,61 @@ class LocationResource(Resource):
         method to provide reverse uri for service call
         '''
         return ""
+
+
+class GetLeadResource(Resource):
+    """
+    API for Requesting PayoffQuote from any 3rd party Application
+    """
+
+    class Meta:
+        '''
+        meta class for resource
+        '''
+        object_class = LeadRequestManager
+        resource_name = 'get'
+        include_resource_uri = False
+        always_return_data = True
+        allowed_methods = ['get']
+        default_format = 'application/json'
+
+    def obj_get_list(self, request=None, **kwargs):
+        '''
+        This method is when a user tries to send request without
+        any Lead Reference Id
+        '''
+        return {'error': 'Value Expected'}
+
+    def obj_get(self, request=None, **kwargs):
+        '''
+        Overriding Tastypie Obj Create Method
+        Get the Records from the db
+        '''
+        self._meta.default_format = 'application/json'
+        lead = self._meta.object_class().get_lead(kwargs['pk'])
+        if not lead:
+            return {'error': 'No Records Found'}
+
+        return lead
+
+    def dehydrate(self, bundle):
+        '''
+        dehydrating the bundle object and copying it to the bundle data
+        for serialization
+        '''
+        bundle.data = bundle.obj.__dict__
+        return bundle
+
+    def alter_detail_data_to_serialize(self, request, data):
+        '''
+        deleting the unwanted key(s) from the object
+        '''
+        del(data.data['_sa_instance_state'])
+
+        return data
+
+    def get_resource_uri(self, bundle_or_obj=None, url_name='api_dispatch_list'):
+        '''
+        method to provide reverse uri for service call
+        '''
+        return ""
